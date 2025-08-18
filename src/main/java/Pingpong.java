b0cebdd3bdf93638bbace20cb37b238d1919958c
Brandon Low <low_brandon@u.nus.edu>
2025-08-18 12:24:36 +0800

    Level-1: Add echo functionality

--- src/main/java/Pingpong.java
+++ src/main/java/Pingpong.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Pingpong {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Pingpong");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        while (!(input = scanner.nextLine()).equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
         System.out.println("____________________________________________________________");
+        System.out.println(" Bye, hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+        scanner.close();
     }
 }
