84f1365ba08b14d1a129c5a74bdce56dff19bc80
Brandon Low <low_brandon@u.nus.edu>
2025-08-18 12:47:17 +0800

    Add support for list

--- src/main/java/Pingpong.java
+++ src/main/java/Pingpong.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Pingpong {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input;
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Pingpong");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        String input;
         while (!(input = scanner.nextLine()).equals("bye")) {
             System.out.println("____________________________________________________________");
             System.out.println(" " + input);
